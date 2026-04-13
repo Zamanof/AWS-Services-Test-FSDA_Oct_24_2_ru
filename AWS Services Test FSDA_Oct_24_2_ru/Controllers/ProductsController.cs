9748bedc1da0245182255ebfd20899d3529e7ebf
Zamanov Nadir Rasim <Zamanov@itstep.edu.az>
2026-04-13 18:10:45 +0400

    Aws

---
+++ AWS Services Test FSDA_Oct_24_2_ru/Controllers/ProductsController.cs
@@ -0,0 +1,47 @@
+﻿using AWS_Services_Test_FSDA_Oct_24_2_ru.Data;
+using AWS_Services_Test_FSDA_Oct_24_2_ru.DTOs;
+using AWS_Services_Test_FSDA_Oct_24_2_ru.Models;
+using AWS_Services_Test_FSDA_Oct_24_2_ru.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AWS_Services_Test_FSDA_Oct_24_2_ru.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductsController : ControllerBase
+{
+    private readonly ApplicationDbContext _context;
+    private readonly IStorage _storage;
+
+    public ProductsController(ApplicationDbContext context, IStorage storage)
+    {
+        _context = context;
+        _storage = storage;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<ProductReadDto>>> GetProducts()
+    {
+        var products = await _context.Products
+                                    .OrderByDescending(p => p.CreatedAt)
+                                    .Select(p => MapToReadDto(p))
+                                    .ToListAsync();
+        return Ok(products);
+    }
+
+    private static ProductReadDto MapToReadDto(Product p)
+    {
+        return new ProductReadDto
+        {
+            Id = p.Id,
+            Name = p.Name,
+            Description = p.Description,
+            Price = p.Price,
+            Category = p.Category,
+            ImageUrl = p.ImageUrl,
+            CreatedAt = p.CreatedAt
+        };
+    }
+}
---
+++ AWS Services Test FSDA_Oct_24_2_ru/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using AWS_Services_Test_FSDA_Oct_24_2_ru.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AWS_Services_Test_FSDA_Oct_24_2_ru.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions options) 
+        : base(options)
+    {}
+    public DbSet<Product> Products => Set<Product>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Product>(
+            entity =>
+            {
+                entity.HasKey(p => p.Id);
+                entity.Property(p => p.Price).HasColumnType("decimal(10, 2)");
+            });
+    }
+
+}
